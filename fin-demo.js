11f13d79078f2a1cb376729c21bfb706978a13af
Goddy-2024 <gaoajuoga@gmail.com>
2024-11-07 01:37:39 +0300

    Add files via upload

---
+++ fin-demo.js
@@ -0,0 +1,19 @@
+        function validatelogin(){
+            var username = document.getElementById('username').value;
+            var password = document.getElementById('password').value;
+            var error = document.getElementById('error');
+            if(username === "########" && password ==="####****")
+        {
+            window.location.href = "dashboard.html";
+        } else{
+            error.innerText = "invalid username or password!";
+            
+        }
+
+     
+        }
+        function showpopup(){
+            document.getElementById('popup').style.display = 'flex';
+            var ID = document.getElementById('userid');
+            
+        }
