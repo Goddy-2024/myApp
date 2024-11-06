4d270ae0b6ffb6686326225cf813a7c0a96cd9cf
Goddy-2024 <gaoajuoga@gmail.com>
2024-11-07 02:35:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
             
         }
 
-        closePopup(){
+     function closePopup(){
                document.getElementById('popup').style.display = 'none';
         }
         function showpopup(){
